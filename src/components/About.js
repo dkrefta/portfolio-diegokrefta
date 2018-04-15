4ea2b46d65445526712d4e3c6865eabe3c0e0b2f
dkrefta <diego.krefta@gmail.com>
2018-04-15 20:18:07 -0300

    Adjuste About

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react'
-import  {Grid, Col, Image } 'react-bootstrap'
+import  {Grid, Col, Image } from 'react-bootstrap'
 import './About.css'
 
 export default class About extends Component {
    render () {
      return (
        <div>
-         ABOUT PAGE
-        <h1> AQQWQOEJPQOWIEQ </h1>
+      <img src="assets/ailymotto.com.png" className="header-image"/>
+    <Grid>
+    <Col xs={12} sm={8} smOffeset={2}>
+    <Image src="assets/diegokrefta.jpg" className="about-profile-pic" rounded />
+  <h3> Diego Krefta </h3>
+<p> This is truly an almighty mountain. We have no limits to our world. We're only limited by our imagination. These little son of a guns hide in your brush and you just have to push them out. I like to beat the brush. </p>
+<p>The little tiny Tim easels will let you down. If you overwork it you become a cloud killer. There's nothing worse than a cloud killer.</p>
+    </Col>
+     </Grid>
        </div>
      );
    }

35574222d02aa534114d3f05f9e8c45cc1c7876c
dkrefta <diego.krefta@gmail.com>
2018-04-15 18:58:32 -0300

    Add Components and Path

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import './App.css';
+import Home from './components/Home'
+import News from './components/News'
+import About from './components/About'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div>
+        <Route exact path="/" component={Home}/>
+        <Route path="about" component={About}/>
+        <Route path="news" component={News}/>
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {Link } from 'react-router-dom';
+import {Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
+import './Home.css'
+
+export default class Home extends Component {
+   render (){
+     return (
+       <Grid>
+         <Jumbotron>
+           <h2> Welcome to my Portfolio </h2>
+           <p> React Engineer </p>
+           </Jumbotron>
+           <Link to="/about">
+             <Button Sstyle='primary'> About </Button>
+           </Link>
+       </Grid>
+     )
+   }
+}

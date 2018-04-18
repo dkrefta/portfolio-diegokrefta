e3031d47a8cc1d97c059395f39696d66dcad3e5d
dkrefta <diego.krefta@gmail.com>
2018-04-17 21:16:24 -0300

    Update Code

--- src/theme/grid.js
+++ src/theme/grid.js
@@ -1,7 +1,18 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+import media from '../theme/media'
 
 export const Container = styled.div`
   padding-left: 120px;
   padding-right: 120px;
   padding-top: 30px;
+  ${media.tablet`
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-top: 15px;
+  `}
+  ${media.phone`
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 15px;
+  `}
 `;
